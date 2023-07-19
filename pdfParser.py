d649a2a7701ab3a7f31c9ec18dffd5c8559ccbe2
JasonBMa <JasonBMa@users.noreply.github.com>
2023-07-19 15:52:41 -0700

    Added correct name, and unit data stored

--- pdfParser.py
+++ pdfParser.py
@@ -1,8 +1,9 @@
 from PyPDF2 import PdfReader
 import re
 
-reader = PdfReader("./GenCat20232024.pdf")
-number_of_pages = len(reader.pages)
+# pdfFileName = "./GenCat20232024.pdf" #Insert PDF_Path here (My use: GenCat20232024.pdf")
+# reader = PdfReader(pdfFileName)
+# number_of_pages = len(reader.pages)
 
 # pdfExtract = open("pdfExtract.txt","w+",encoding="utf-8")
 
@@ -15,15 +16,26 @@ number_of_pages = len(reader.pages)
 
 #Opening file just opened
 matches = []
-courses = open("courses.txt","w+",encoding="utf-8")
-courses.write("SUBJECT,COURSE_NUMBER,NAME")
+filter = open("filtered_PDF_Extract.txt","w+",encoding="utf-8") #Containg course details
 with open("pdfExtract.txt","r",encoding="utf-8") as file:
+    fileLines = file.read()
+    pattern = re.compile("[A-Z]{3} [0-9]{3}[A-Z]* —\xa0.+\)")
+    matches = pattern.findall(fileLines)
+    print(matches)
+    for match in matches:
+        filter.write(match+"\n")
+        # output.write(match.group(1) + "," + match.group(2) + "," + match.group(3) + ",")
+filter.close()
+
+output = open("CoursesOffered.txt","w+",encoding="utf-8") #Containg course details
+output.write("SUBJECT,COURSE_NUMBER,NAME,UNIT_COUNT\n")
+with open("filtered_PDF_Extract.txt","r",encoding="utf-8") as file:
     fileLines = file.readlines()
     for line in fileLines:
-        match = re.match(r"(\b[A-Z]{3}\b) (\b[0-9]{3}[A-Z]*\b) —\xa0(\b.*\b)",line)
+        match = re.match(r"(\b[A-Z]{3}\b) (\b[0-9]{3}[A-Z]*\b) —\xa0(\b.*\b) \((\b.*\b)\)",line)
         if(match != None):
-            courses.write(match.group(1)+","+match.group(2)+","+match.group(3)+'\n')
-courses.close()
+            output.write(match.group(1)+","+match.group(2)+","+match.group(3)+","+match.group(4)+'\n')
+output.close()
 
 # with open("courses.txt","w+",encoding="utf-8") as file:
 #     for i in matches:
