a731041953969e36ec77e187693e6f3ff3f3525e
JasonBMa <JasonBMa@users.noreply.github.com>
2023-07-20 04:51:59 -0700

    Updated so it outputs a json instead of text file so it be imported into MongoDB

--- pdfParser.py
+++ pdfParser.py
@@ -1,6 +1,8 @@
-from PyPDF2 import PdfReader
-import re
+from PyPDF2 import PdfReader #turns pdf into string
+import re #Used for parsing what we need from pdf
+import json #Need for output to enter into database
 
+## Commented out because, I only needed to read from pdf file once.
 # pdfFileName = "./GenCat20232024.pdf" #Insert PDF_Path here (My use: GenCat20232024.pdf")
 # reader = PdfReader(pdfFileName)
 # number_of_pages = len(reader.pages)
@@ -16,7 +18,7 @@ import re
 
 #Opening file just opened
 matches = []
-filter = open("filtered_PDF_Extract.txt","w+",encoding="utf-8") #Containg course details
+filter = open("filtered_PDF_Extract.txt","w+",encoding="utf-8") #Containing course details
 with open("pdfExtract.txt","r",encoding="utf-8") as file:
     fileLines = file.read()
     pattern = re.compile("[A-Z]{3} [0-9]{3}[A-Z]* —\xa0.+\)")
@@ -27,16 +29,23 @@ with open("pdfExtract.txt","r",encoding="utf-8") as file:
         # output.write(match.group(1) + "," + match.group(2) + "," + match.group(3) + ",")
 filter.close()
 
-output = open("CoursesOffered.txt","w+",encoding="utf-8") #Containg course details
-output.write("SUBJECT,COURSE_NUMBER,NAME,UNIT_COUNT\n")
+array = []
+id = 0
 with open("filtered_PDF_Extract.txt","r",encoding="utf-8") as file:
     fileLines = file.readlines()
     for line in fileLines:
         match = re.match(r"(\b[A-Z]{3}\b) (\b[0-9]{3}[A-Z]*\b) —\xa0(\b.*\b) \((\b.*\b)\)",line)
+        dict2 = {}
         if(match != None):
-            output.write(match.group(1)+","+match.group(2)+","+match.group(3)+","+match.group(4)+'\n')
-output.close()
+            fields = dict()
+            #output.write(match.group(1)+","+match.group(2)+","+match.group(3)+","+match.group(4)+'\n')
+            fields["subject"] = match.group(1)
+            fields["subject_code"] = match.group(2)
+            fields["name"] = match.group(3)
+            fields["units"] = match.group(4)
+            array.append(fields)
+        id+=1
 
-# with open("courses.txt","w+",encoding="utf-8") as file:
-#     for i in matches:
-#         file.write(i.group(1) + "," + i.group(2)+"," + i.group(3))
+#Outputs in a array holding objects, used to import into mongoDB
+with open("CoursesOffered.json","w+",encoding="utf-8") as output: #Containing course details
+    json.dump(array, output, indent = 4, sort_keys = False)
